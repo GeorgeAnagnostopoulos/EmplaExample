c3847a0c0ce299253fe3597ce24f58e1e67f633c
George Anagnostopoulos <anagnostopoulos.george@gmail.com>
2023-05-29 07:39:28 -0700

    Initial commit

---
+++ src/js/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import EmblaCarousel from './EmblaCarousel'
+import { EmblaOptionsType } from 'embla-carousel-react'
+import Header from './Header'
+import Footer from './Footer'
+import '../css/base.css'
+import '../css/sandbox.css'
+import '../css/embla.css'
+
+const OPTIONS: EmblaOptionsType = {
+  slidesToScroll: 'auto',
+  containScroll: 'trimSnaps',
+}
+const SLIDE_COUNT = 8
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
+const App: React.FC = () => (
+  <main className="sandbox">
+    <Header />
+    <section className="sandbox__carousel">
+      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+    </section>
+    <Footer />
+  </main>
+)
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
